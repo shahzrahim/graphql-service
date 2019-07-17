0a739c8571adf2bc590744dc8fd241a0ace812e0
shazi <shaz.rahim@yahoo.com>
2019-07-17 15:33:53 -0500

    updated scheme to have two-way relationships between users and posts and posts and comments

--- src/index.js
+++ src/index.js
@@ -9,23 +9,73 @@ import { GraphQLServer } from 'graphql-yoga'
 
 
 // Demo User Data
-const users = [{
+const users = [
+    {
+        id: "1",
+        name: "Shahzi",
+        email: "shaz@aol.com",
+        comments: ["1123", "4322", "3342"]
+    },
+    {
+        id: "2",
+        name: "Mary",
+        email: "Mary@aol.com",
+        age: 24,
+        comments:[],
+    },
+    {
+        id: "3",
+        name: "Scott",
+        email: "Scotry@aol.com",
+        age: 24,
+        comments: ["5123"]
+    }
+]
+
+const myPosts = [{
     id: "1",
-    name: "Shahzi",
-    email: "shaz@aol.com",
-},
-{
-  id: "2",
-  name: "Mary",
-  email: "Mary@aol.com",
-  age: 24,
-},
-{
-  id: "3",
-  name: "Scott",
-  email: "Scotry@aol.com",
-  age: 24,
-}
+    title: "Post #1",
+    body: "shaz@aol.com",
+    published: true,
+    author: "1"
+  },
+  {
+    id: "2",
+    title: "Post #2",
+    body: "body of post 2",
+    published: true,
+    author: "1"
+  },
+  {
+    id: "3",
+    title: "Post #3",
+    body: "body of post 3",
+    published: false,
+    author: "3"
+  }
+]
+
+const myComments = [
+    {
+        id: "1123",
+        text: "ShahzComi",
+        author: "1"
+    },
+    {
+        id: "4322",
+        text: "MaryComit",
+        author: "1"
+    },
+    {
+        id: "3342",
+        text: "ScottCommit",
+        author: "1"
+    },
+    {
+        id: "5123",
+        text:"brobrobroooooo",
+        author: "2"
+    }
 ]
 
 
@@ -33,22 +83,30 @@ const users = [{
 const typeDefs = `
     type Query {
         users(query: String): [User!]!
+        myPosts(query: String): [Post!]!
         me: User!
         posts: Post!
+        comments: [Comment!]!
     }
-
     type User {
         id: ID!
         name: String!
         email: String!
         age: Int
+        posts: [Post!]!
+        comments: [Comment!]!
     }
-
     type Post {
         id: ID!
         title: String!
         body: String!
         published: String
+        author: User!
+    }
+    type Comment {
+        id: ID!
+        text: String!
+        author: User!
     }
 `
 
@@ -66,14 +124,49 @@ const resolvers = {
                 age: null
             }
         },
-        posts() {
-            return {
-                id: "as32423123124123",
-                title:"The day I finished my post",
-                body:"leps  sxfsdfsdfs  sdfsdf",
+        myPosts(parent, args, crx, info) {
+            if (!args.query) {
+                return myPosts
             }
+            return myPosts.filter((post) => {
+                const titleMatch = post.title.toLowerCase().includes(args.query.toLowerCase())
+                const bodyMatch = post.body.toLowerCase().includes(args.query.toLowerCase())
+                return titleMatch || bodyMatch;
+            })
         },
+        comments(parent, args, crx, info) {
+            return myComments
+        }
+    },
+    User: {
+        posts(parent, args, crx, info) {
+            console.log(myPosts);
+
+            return myPosts.filter((post) =>{
+                return post.author === parent.id
+            })
+        },
+        comments(parent, args, crx, info) {
+            return myComments.filter((comment) => {
+                return comment.author === parent.id
+            })
+        }
+    },
+    Post: {
+        author(parent, args, crx, info) {
+            return users.find((user) => {
+                return user.id === parent.author
+            })
+        }
+    },
+    Comment: {
+        author(parent, args, crx, info) {
+          return users.find((user) => {
+            return user.id === parent.author
+          })
+        }
     }
+
 }
 
 const server = new GraphQLServer({

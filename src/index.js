d6738a5a83d12a933cc369b99ebaf464b52ff689
shazi <shaz.rahim@yahoo.com>
2019-07-18 18:41:33 -0500

    completed dummy subscription

--- src/index.js
+++ src/index.js
@@ -1,33 +1,27 @@
-import { GraphQLServer } from 'graphql-yoga'
-import uuidv4 from 'uuid/v4'
+import { GraphQLServer, PubSub } from 'graphql-yoga'
 import db from './db'
-import Query from './resolvers/Query';
-import Mutation from './resolvers/Mutation';
-import Post from './resolvers/Post';
-import User from './resolvers/User';
-import Comment from './resolvers/Comment';
+import Query from './resolvers/Query'
+import Mutation from './resolvers/Mutation'
+import Subscription from './resolvers/Subscription'
+import User from './resolvers/User'
+import Post from './resolvers/Post'
+import Comment from './resolvers/Comment'
 
-//Bootstrap application
-// Resolvers
-const resolvers = {
-    Query,
-    Mutation,
-    Post,
-    User,
-    Comment
-}
+const pubsub = new PubSub()
 
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
     resolvers: {
-      Query,
-      Mutation,
-      Post,
-      User,
-      Comment
+        Query,
+        Mutation,
+        Subscription,
+        User,
+        Post,
+        Comment
     },
     context: {
-        db
+        db,
+        pubsub
     }
 })
 

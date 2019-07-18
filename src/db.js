ca31418c4bd89d0ab5db483fb62d995f47bf055f
shazi <shaz.rahim@yahoo.com>
2019-07-18 15:57:35 -0500

    appropriate structure including reducers directory

--- src/db.js
+++ src/db.js
@@ -1,75 +1,64 @@
 const users = [{
-    id: "1",
-    name: "Shahzi",
-    email: "shaz@aol.com",
-    comments: ["1123", "4322", "3342"]
-  },
-  {
-    id: "2",
-    name: "Mary",
-    email: "Mary@aol.com",
-    age: 24,
-    comments: [],
-  },
-  {
-    id: "3",
-    name: "Scott",
-    email: "Scotry@aol.com",
-    age: 24,
-    comments: ["5123"]
-  }
-]
+    id: '1',
+    name: 'Andrew',
+    email: 'andrew@example.com',
+    age: 27
+}, {
+    id: '2',
+    name: 'Sarah',
+    email: 'sarah@example.com'
+}, {
+    id: '3',
+    name: 'Mike',
+    email: 'mike@example.com'
+}]
 
-const myPosts = [{
-    id: "1",
-    title: "Post #1",
-    body: "shaz@aol.com",
+const posts = [{
+    id: '10',
+    title: 'GraphQL 101',
+    body: 'This is how to use GraphQL...',
     published: true,
-    author: "1",
-  },
-  {
-    id: "2",
-    title: "Post #2",
-    body: "body of post 2",
-    published: true,
-    author: "1"
-  },
-  {
-    id: "3",
-    title: "Post #3",
-    body: "body of post 3",
+    author: '1'
+}, {
+    id: '11',
+    title: 'GraphQL 201',
+    body: 'This is an advanced GraphQL post...',
     published: false,
-    author: "3"
-  }
-]
+    author: '1'
+}, {
+    id: '12',
+    title: 'Programming Music',
+    body: '',
+    published: true,
+    author: '2'
+}]
 
-const myComments = [{
-    id: "1123",
-    text: "ShahzComi",
-    author: "1",
-    postId: "1"
-  },
-  {
-    id: "4322",
-    text: "MaryComit",
-    author: "1",
-    postId: "2"
-  },
-  {
-    id: "3342",
-    text: "ScottCommit",
-    author: "1",
-    postId: "1"
-  },
-  {
-    id: "5123",
-    text: "brobrobroooooo",
-    author: "2",
-    postId: "3"
-  }
-]
-export default db = {
-  users,
-  posts,
-  comments
+const comments = [{
+    id: '102',
+    text: 'This worked well for me. Thanks!',
+    author: '3',
+    post: '10'
+}, {
+    id: '103',
+    text: 'Glad you enjoyed it.',
+    author: '1',
+    post: '10'
+}, {
+    id: '104',
+    text: 'This did no work.',
+    author: '2',
+    post: '11'
+}, {
+    id: '105',
+    text: 'Nevermind. I got it to work.',
+    author: '1',
+    post: '12'
+}]
+
+const db = {
+    users,
+    posts,
+    comments
 }
+
+export { db as default }
--- src/index.js
+++ src/index.js
@@ -1,178 +1,18 @@
 import { GraphQLServer } from 'graphql-yoga'
-import uuidv4 from 'uuid/v4';
+import uuidv4 from 'uuid/v4'
 import db from './db'
-
+import Query from './resolvers/Query';
+import Mutation from './resolvers/Mutation';
+import Post from './resolvers/Post';
+import User from './resolvers/User';
+import Comment from './resolvers/Comment';
 // Resolvers
 const resolvers = {
-    Query: {
-        users(parent, args, {db}, info) {
-            return (!args.query) ? db.users : db.users.filter((user) => user.name.toLowerCase().includes(args.query.toLowerCase()))
-        },
-        me() {
-            return {
-                id: '123',
-                name: "mike",
-                email: "shaz@aol.com",
-                age: null
-            }
-        },
-        posts(parent, args, {db}, info) {
-            if (!args.query) {
-                return db.myPosts
-            }
-            return db.myPosts.filter((post) => {
-                const titleMatch = post.title.toLowerCase().includes(args.query.toLowerCase())
-                const bodyMatch = post.body.toLowerCase().includes(args.query.toLowerCase())
-                return titleMatch || bodyMatch;
-            })
-        },
-        comments(parent, args, {db}, info) {
-            return db.myComments
-        }
-    },
-    Mutation: {
-        createUser(parents, args, {db}, info) {
-            const emailTaken = db.users.some((user) => user.email === args.data.email)
-
-            if(emailTaken) {
-                throw new Error('Email Taken')
-            }
-
-            const user = {
-                id: uuidv4(),
-                ...args.data
-            }
-
-            db.users.push(user)
-
-            return user
-        },
-        deleteUser(parents, args, {db}, info) {
-            const userIndex = db.users.findIndex((user) => user.id === args.id)
-
-            if (userIndex === -1) {
-                throw new Error("User does not exist")
-            }
-
-            const updatedUsers = db.users.splice(userIndex, 1)
-
-            db.myPosts = db.myPosts.filter((post) => {
-                const match = post.author === args.id
-
-                if(!match) {
-                    db.myComments = db.myComments.filter((comment) => comment.post !== db.post.id)
-                }
-
-                return !match
-            })
-
-            db.myComments = db.myComments.filter((comment) => comment.author !== args.id)
-            return updatedUsers[0]
-        },
-        createPost(parents, args, {db}, info) {
-            const userExists = db.users.some((user) => user.id === args.data.author)
-
-            if(!userExists) {
-                throw new Error("User not found")
-            }
-            const post = {
-                id: uuidv4(),
-                ...args.data
-            }
-
-            db.myPosts.push(post)
-
-            return post
-        },
-        deletePost(parents, args, {db}, info) {
-            const postExists = db.myPosts.some((post) =>  post.id === args.id)
-            const postIndex = db.myPosts.findIndex((post) => post.id === args.id)
-
-
-            if(!postExists) {
-                throw new Error('Post does not exist')
-            }
-
-
-
-            const deletedPost = db.myPosts.splice(postIndex, 1)
-
-            db.myComments = db.myComments.filter((comment) => {
-                return comment.postId !== args.id
-            })
-
-            return deletedPost[0];
-        },
-        createComment(parents, args, {db}, info) {
-
-            const userExists = db.users.some((user) => user.id === args.data.author)
-            const postExists = db.myPosts.some((post) => post.id === args.data.post && post.published)
-
-            if (!userExists || !postExists) {
-              throw new Error('Unable to find user and post')
-            }
-
-            const comment = {
-                id: uuidv4(),
-                ...args.data
-            }
-
-            db.myComments.push(comment);
-
-            return comment
-        },
-        deleteComment(parents, args, {db}, info) {
-            const commentExists = db.myComments.some((comment) => comment.id === args.id)
-            const commentIndex = db.myComments.findIndex((comment) => comment.id === args.id)
-
-            if (!commentExists) {
-                throw new Error('Unable to find comment')
-            }
-            const deletedComment = db.myComments.splice(commentIndex, 1)
-
-
-            return deletedComment[0];
-
-        }
-    },
-    User: {
-        posts(parent, args, {db}, info) {
-            return db.myPosts.filter((post) =>{
-                return post.author === parent.id
-            })
-        },
-        comments(parent, args, {db}, info) {
-            return db.myComments.filter((comment) => {
-                return comment.author === parent.id
-            })
-        }
-    },
-    Post: {
-        author(parent, args, {db}, info) {
-            return db.users.find((user) => {
-                return db.user.id === parent.author
-            })
-        },
-        comments(parent, args, {db}, info) {
-            return db.myComments.filter((comment) => {
-                return comment.postId === parent.id
-            })
-        }
-    },
-    Comment: {
-        author(parent, args, {db}, info) {
-          return db.users.find((user) => {
-            return user.id === parent.author
-          })
-        },
-        post(parent, args, {db}, info) {
-
-          return db.myPosts.find((post) => {
-            return post.id  === parent.postId
-          })
-        },
-    }
-
+    Query,
+    Mutation,
+    Post,
+    User,
+    Comment
 }
 
 const server = new GraphQLServer({
